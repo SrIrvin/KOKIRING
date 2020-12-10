cadfaa8d3e0b0550e0daee0c82c8be8130e81db4
unknown <sr._irvin@hotmail.com>
2020-12-09 21:37:35 -0600

    Pre-Alphav0-04
    Etapa de pruebas de producción
    
    -Mejorarón controles
    -Se mdifica el mapa

--- kokiring/Assets/Ply/MovPly.cs
+++ kokiring/Assets/Ply/MovPly.cs
@@ -38,36 +38,63 @@ public class MovPly : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         Vector3 dir = new Vector3(horizontal, 0.0f, vertical).normalized;
+        if (Input.GetKey(KeyCode.LeftShift ) && vel<60) {
+            vel += 1;
+        } else vel = 20;
 
         //Animaciones
         anim.SetBool("caminar", dir.magnitude >= 0.1f);
-        anim.SetBool("jump",  DiretionY>-5 && Input.GetButtonDown("Jump"));
-
+        //anim.SetBool("jump",  DiretionY>-5 && Input.GetButtonDown("Jump"));
+        anim.SetBool("jump", !chrCtrl.isGrounded);
 
 
         if (chrCtrl.isGrounded) { grav = 0.1F; DiretionY = -0.1f; }
         else grav = 9.3f;
 
 
-        if (dir.magnitude >= 0.1f || !chrCtrl.isGrounded) {
-            
-            float targetangle = Mathf.Atan2(dir.x, dir.y) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angl = Mathf.SmoothDampAngle(tr.eulerAngles.y, targetangle,ref turnSmoothVel, turnSmoothTime);
+        if (dir.magnitude >= 0.1f || !chrCtrl.isGrounded)
+        {
+            float targetangle;
+            if (Input.GetAxis("Vertical") < 0.0f)
+            {
+                targetangle = Mathf.Atan2(-dir.x, -dir.y) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            }
+            else
+            {
+                targetangle = Mathf.Atan2(dir.x, dir.y) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            }
+            float angl = Mathf.SmoothDampAngle(tr.eulerAngles.y, targetangle, ref turnSmoothVel, turnSmoothTime);
             Vector3 moveDir = Quaternion.Euler(0.0f, targetangle, 0.0f) * Vector3.forward;
 
-            if (Input.GetButtonDown ("Jump") && chrCtrl.isGrounded )
+            if (Input.GetButtonDown("Jump") && chrCtrl.isGrounded)
             {
                 DiretionY = jumpspeed;
             }
 
-            if(DiretionY>-5.2f )
-            DiretionY -= grav *Time.deltaTime ;
+
+            if (DiretionY > -5.2f)
+                DiretionY -= grav * Time.deltaTime;
 
             moveDir.y = DiretionY;
 
             tr.transform.rotation = Quaternion.Euler(0.0f, angl, 0.0f);
             chrCtrl.Move(moveDir.normalized * vel * Time.deltaTime);
         }
+        else {
+            if (Input.GetButtonDown("Jump") && chrCtrl.isGrounded)
+            {
+                DiretionY = jumpspeed;
+                anim.Play("callendo");
+            }
+
+
+            if (DiretionY > -5.2f)
+                DiretionY -= grav * Time.deltaTime;
+
+            chrCtrl.Move(new Vector3(0.0f, DiretionY, 0.0f) * vel * Time.deltaTime);
+
+        }
+       
         
 
     }
